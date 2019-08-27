a58c038e73d29341355d4f9d0123eeb11047593b
Andrew <andrew.j.hui@gmail.com>
2019-08-27 19:19:56 +0000

    adding the typewriter function

---
+++ d2/typewriter.js
@@ -0,0 +1,9 @@
+const sentence = "hello there from lighthouse labs";
+
+let count = 1000;
+for (const char of sentence) {
+  count += 50
+  setTimeout(() => {
+    process.stdout.write(char + '\n');    
+  }, count)
+}
