ad0f0f84445da36c755a11b3c9579dd6726e155a
Andrew <andrew.j.hui@gmail.com>
2019-08-29 22:52:43 +0000

    json the cat

--- d4-api/json_the_cat/breedFetcher.js
+++ d4-api/json_the_cat/breedFetcher.js
@@ -1,24 +1,24 @@
 const request = require('request');
-const input = process.argv.splice(2)[0];
-const URL = `https://api.thecatapi.com/v1/breeds/search/?q=${input}`;
 
-request(URL, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
+const fetchBreedDescription = function(input, callback) {
 
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("Requested breed not found");
-    return;
-  }
-  console.log(data[0].description);
-  //console.log(typeof data);
+  const URL = `https://api.thecatapi.com/v1/breeds/search/?q=${input}`;
 
-  //console.log("err", error);
-  //console.log("response", response);
-  //console.log("body", body);
+  request(URL, (error, response, body) => {
+    if (error) {
+      return callback(error);
+    }
 
-});
+    const data = JSON.parse(body);
 
+    if (data.length === 0) {
+      return callback("Requested breed not found");
+    }
+    callback(error, data[0].description.trim());
+  });
+
+};
+
+
+
+module.exports = {fetchBreedDescription};
