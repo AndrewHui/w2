07ca8898a6e3b549d0732a1f01a6ec39df26248c
Andrew <andrew.j.hui@gmail.com>
2019-08-29 23:28:01 +0000

    commit the iss spotter 1

---
+++ d4-api/iss_spotter/iss.js
@@ -0,0 +1,35 @@
+const request = require('request');
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const ipURL = "https://api.ipify.org?format=json";
+
+const fetchMyIP = function(callback) {
+  // use request to fetch IP address from JSON API
+  request(ipURL, (error, response, body) => {
+    if (error) {
+      return callback(error);
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const data = JSON.parse(body);
+
+    return callback(error, data.ip);
+
+
+  });
+};
+
+module.exports = { fetchMyIP };
+
